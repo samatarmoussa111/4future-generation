634135e06d9f067608b398100c8cbda3322074b1
Samatar Moussa <samatarmoussa111@gmail.com>
2024-08-23 14:40:52 -0400

    test:test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Hero from "@/components/hero/hero";
 import Navigation from "@/components/navigation/navigation";
 import Wrapper from "@/components/wrapper/wrapper";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetch("http://127.0.0.1:1337/api/posts").then(
+    (response) => response.json()
+  );
   return (
     <>
       <Wrapper>
@@ -11,6 +14,11 @@ export default function Home() {
       </Wrapper>
       <Banner />
       <Hero />
+      {posts.data.map((post: any) => (
+        <div key={post.id}>
+          <h1>{post.attributes.title}</h1>
+        </div>
+      ))}
     </>
   );
 }
