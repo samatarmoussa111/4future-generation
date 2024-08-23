c1ebcf42cb4f25a4afc473da3a90b75f07bfb4b8
Samatar Moussa <samatarmoussa111@gmail.com>
2024-08-23 14:44:31 -0400

    feat: Framer motion installation

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import Navigation from "@/components/navigation/navigation";
 import Wrapper from "@/components/wrapper/wrapper";
 
 export default async function Home() {
-  const posts = await fetch("http://127.0.0.1:1337/api/posts").then(
-    (response) => response.json()
-  );
   return (
     <>
       <Wrapper>
@@ -14,11 +11,6 @@ export default async function Home() {
       </Wrapper>
       <Banner />
       <Hero />
-      {posts.data.map((post: any) => (
-        <div key={post.id}>
-          <h1>{post.attributes.title}</h1>
-        </div>
-      ))}
     </>
   );
 }
